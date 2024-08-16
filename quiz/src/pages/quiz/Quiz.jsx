be3c0e769583dfe50f0eb641e46d05a0135d0848
Şüheda <92635912+SuhedaTaspinar@users.noreply.github.com>
2024-08-16 22:26:11 +0300

    implement quiz functionality with QuestionCard component and api integration

--- quiz/src/pages/quiz/Quiz.jsx
+++ quiz/src/pages/quiz/Quiz.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./Quiz.css"
 import {useParams} from "react-router-dom";
+import * as api from "../../api/api.js"
+import QuestionCard from "../../components/questionCard/QuestionCard.jsx";
 
 const Quiz = () => {
 
     const {difficulty, amount} = useParams()
+    const [questionsData, setQuestionsData] = useState([])
+    const [score, setScore] = useState(0)
+    const [count, setCount] = useState(0)
+    const [modal, setModal] = useState(false)
+
+    useEffect(() => {
+        const getData = async() => {
+            const data = await api.fetchQuizData(difficulty, amount)
+            setQuestionsData(data)
+        }
+        getData();  // getData fonksiyonunu burada çağırman gerekiyor
+    }, [difficulty, amount]);  // Ayrıca dependency array'e difficulty ve amount'u ekledim
+
+    console.log(questionsData, "questionsData")
 
     return(
         <div className="quiz">
-            {difficulty} - {amount}
+            <QuestionCard
+                questionsData={questionsData}
+                score={score}
+                setScore={setScore}
+                count={count}
+                setCount={setCount}
+                modal={modal}
+                setModal={setModal}
+            />
         </div>
     )
 }
